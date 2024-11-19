831d0b4513ef53d2866511599f164be885dba76d
fabius <fabioclf@gmail.com>
2024-11-19 15:24:37 -0400

    aplicando correções

--- src/containers/FormBusca/index.tsx
+++ src/containers/FormBusca/index.tsx
@@ -1,18 +1,62 @@
+import React, { useEffect, useState } from 'react'
 import * as S from './styles'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
-const FormBusca = () => (
-  <>
-    <h2>Pesquisa</h2>
-    <S.Form>
-      <label>Busca por nome:</label>
-      <input type="text" placeholder="Digite o nome" />
-      <label>Busca por email:</label>
-      <input type="email" placeholder="Digite o e-mail" />
-      <label>Busca por telefone:</label>
-      <input type="tel" placeholder="Digite o telefone" />
-      <button>Pesquisar</button>
-    </S.Form>
-  </>
-)
+interface FormBuscaProps {
+  setContatosFiltrados: (contatos: any[]) => void
+}
+
+const FormBusca: React.FC<FormBuscaProps> = ({ setContatosFiltrados }) => {
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefone, setTelefone] = useState('')
+
+  const contatos = useSelector((state: RootReducer) => state.contatos.contatos)
+
+  useEffect(() => {
+    const contatosFiltrados = contatos.filter((contato) => {
+      return (
+        (nome
+          ? contato.nome.toLowerCase().includes(nome.toLowerCase())
+          : true) &&
+        (email
+          ? contato.email.toLowerCase().includes(email.toLowerCase())
+          : true) &&
+        (telefone ? contato.telefone.includes(telefone) : true)
+      )
+    })
+    setContatosFiltrados(contatosFiltrados)
+  }, [nome, email, telefone, contatos, setContatosFiltrados])
+
+  return (
+    <>
+      <h2>Pesquisa</h2>
+      <S.Form>
+        <label>Busca por nome:</label>
+        <input
+          type="text"
+          placeholder="Digite o nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <label>Busca por email:</label>
+        <input
+          type="email"
+          placeholder="Digite o e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Busca por telefone:</label>
+        <input
+          type="tel"
+          placeholder="Digite o telefone"
+          value={telefone}
+          onChange={(e) => setTelefone(e.target.value)}
+        />
+      </S.Form>
+    </>
+  )
+}
 
 export default FormBusca
--- src/containers/PainelLateral/index.tsx
+++ src/containers/PainelLateral/index.tsx
@@ -1,12 +1,17 @@
 import { Painel } from './styles'
 import FormContato from '../FormContato'
 import FormBusca from '../FormBusca'
+import { useState } from 'react'
 
-const PainelLateral = () => (
-  <Painel>
-    <FormBusca />
-    <FormContato />
-  </Painel>
-)
+const PainelLateral = () => {
+  const [contatosFiltrados, setContatosFiltrados] = useState([])
+
+  return (
+    <Painel>
+      <FormBusca setContatosFiltrados={setContatosFiltrados} />
+      <FormContato />
+    </Painel>
+  )
+}
 
 export default PainelLateral
